b3a540f04f0ef4b4e2fecbd1c749acec4a1fbce3
Renaud Hager <renaud.hager@gmail.com>
2017-03-30 18:53:55 +0100

    Create gluster role.

--- site.pp
+++ site.pp
@@ -11,6 +11,7 @@ if empty( hiera( 'role', undef ) )
     /compute-sl\d\d$/:       { $role = 'mesos-slave' }
     /compute-ma\d\d$/:       { $role = 'mesos-master' }
     /dns-\d\d$/:             { $role = 'dns-consul' }
+    /gluster-\d\d$/:         { $role = 'gluster' }
     /puppetdb-pgsql-\d\d$/:  { $role = 'puppetdb-pgsql' }
     /nginx-\d\d$/:           { $role = 'nginx' }
     /puppetca-\d\d$/:        { $role = 'puppetca' }
