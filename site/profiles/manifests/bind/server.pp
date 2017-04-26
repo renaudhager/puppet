c6b23caebfc148eec24104a2c9316878fd3c4029
Renaud Hager <renaud.hager@gmail.com>
2017-04-26 20:07:40 +0100

    Add bind controls statement config file.

--- site/profiles/manifests/bind/server.pp
+++ site/profiles/manifests/bind/server.pp
@@ -10,7 +10,24 @@ class profiles::bind::server (
 )  {
 
   $confs = hiera_hash( 'profiles::bind::server::confs', {} )
+  $controls = hiera_hash( 'profiles::bind::server::controls', {} )
 
   create_resources( bind::server::conf, $confs )
+
+  if ! empty($controls) {
+    file{ '/etc/named/controls':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template("${module_name}/bind/controls.erb"),
+      notify  => Service['named'],
+    }
+  }
+  else {
+    file{ '/etc/named/controls':
+      ensure  => 'absent',
+      notify  => Service['named'],
+    }
+  }
 
 }
