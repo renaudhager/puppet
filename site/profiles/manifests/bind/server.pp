466aafefca0bea0fa377ecba9def892e69729a61
Renaud Hager <renaud.hager@gmail.com>
2017-04-28 18:32:59 +0100

    Remove controls management. Remove consul template related configuration.

--- site/profiles/manifests/bind/server.pp
+++ site/profiles/manifests/bind/server.pp
@@ -14,20 +14,4 @@ class profiles::bind::server (
 
   create_resources( bind::server::conf, $confs )
 
-  if ! empty($controls) {
-    file{ '/etc/named/controls':
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template("${module_name}/bind/controls.erb"),
-      notify  => Service['named'],
-    }
-  }
-  else {
-    file{ '/etc/named/controls':
-      ensure  => 'absent',
-      notify  => Service['named'],
-    }
-  }
-
 }
